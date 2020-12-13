fa8e145fa872c8928bc2df847f510b1f7ece4506
Alfrederson <aroldorosenberg@gmail.com>
2020-12-12 22:56:59 -0300

    Delete instalar.bat

--- instalar.bat
+++
@@ -1,8 +0,0 @@
-echo off
-cls
-echo Atualizando indice de placas
-"tool/arduino-cli.exe" core update-index
-echo Instalando ferramentas arduino
-"tool/arduino-cli.exe" core install arduino:avr
-echo Se tudo tiver dado certo, o programa vai funcionar
-pause
